0f3c122a3f4d920da851b126d6a5f07850061520
Md Nasir uddin <mdnasirdmt@gmail.com>
2023-05-14 19:24:39 +0530

    node

--- may-2023/src/linkedList/NodeCreation.java
+++ may-2023/src/linkedList/NodeCreation.java
@@ -16,31 +16,72 @@ public class NodeCreation {
 		}
 
 	}
-	
-	
+
 	public static void main(String[] args) {
-		NodeCreation creation=new NodeCreation();
-		creation.head= new Node(1);
-		Node sec=new Node(3);
-		Node th= new Node(5);
-		Node fr=new Node(7);
-		
-		head.next=sec;
-		sec.next=th;
-		th.next=fr;
-		
-		printNode();
-		
+		NodeCreation creation = new NodeCreation();
+		creation.head = new Node(1);
+		Node sec = new Node(3);
+		Node th = new Node(5);
+		Node fr = new Node(7);
+		Node fif = new Node(8);
+
+		head.next = sec;
+		sec.next = th;
+		th.next = fr;
+		fr.next = fif;
+
+		printNode(head);
+		System.out.println();
+		printLength(head);
+		printMid(head);
+
 	}
 
+	// print node element
+	private static void printNode(Node head1) {
+		Node current = head1;
 
-	private static void printNode() {
-		Node current= head;
-		
-		while(current!=null) {
-			System.out.print(current.data+" ");
-			current=current.next;
+		while (current != null) {
+			System.out.print(current.data + " ");
+			current = current.next;
 		}
+	}
+
+//		print length element
+	private static void printLength(Node head2) {
+
+		Node cur = head2;
+		int c = 0;
+		while (cur.next != null) {
+			c++;
+			cur = cur.next;
+		}
+		System.out.println("Length is : " + c);
+
+	}
+
+	// print mid
+	private static void printMid(Node head3) {
+		Node cur = head3;
+		int c = 0;
+		while (cur.next != null) {
+			c++;
+			cur = cur.next;
+		}
+		int mid = c / 2;
+//		System.out.println(mid);
+		
+		
+		Node cur1=head3;
+		int i=0;
+		while( i<mid) {
+			
+			cur1=cur1.next;
+			i++;
+		}
+		
+		System.out.println(cur1.data);
+
 	}
 
 }
