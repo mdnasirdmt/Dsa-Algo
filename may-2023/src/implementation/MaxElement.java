6b14640ce79b29dfb1a777e12cf1d7c1b5fa16ff
Md Nasir uddin <mdnasirdmt@gmail.com>
2023-05-14 18:19:17 +0530

    frequency

---
+++ may-2023/src/implementation/MaxElement.java
@@ -0,0 +1,30 @@
+package implementation;
+
+import java.util.Arrays;
+
+public class MaxElement {
+
+	public static void main(String[] args) {
+		
+		int[] arr = { 1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4, 1, 1 };
+		int[] freq=new int[arr.length];
+		for (int i = 0; i < arr.length; i++) {
+			freq[arr[i]]++;
+//			System.out.print(freq[arr[i]]+" ");
+		}
+//		System.out.println();
+		
+//		System.out.println(Arrays.toString(freq));
+		int c=0;
+		int max=0;
+		for (int i = 0; i < freq.length; i++) {
+//			System.out.print(freq[i] +" ");
+			if(freq[i]>max) {
+				max=freq[i];
+			}
+		}
+		System.out.println(max);
+
+	}
+
+}
