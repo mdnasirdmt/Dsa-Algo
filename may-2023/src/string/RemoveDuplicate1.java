7a46a77e1c5f63c742beaf42c89778ca5989fe53
Md Nasir uddin <mdnasirdmt@gmail.com>
2023-05-15 21:18:46 +0530

    occurance

--- may-2023/src/string/RemoveDuplicate1.java
+++ may-2023/src/string/RemoveDuplicate1.java
@@ -4,32 +4,31 @@ import java.util.Arrays;
 
 public class RemoveDuplicate1 {
 	public static void main(String[] args) {
-		String[] str = {"abc", "def", "ghi", "abc", "def"};
-		int c=0;
+		String[] str = { "abc", "def", "ghi", "abc", "def" };
+		int c = 0;
 		Arrays.sort(str);
 		for (int i = 0; i < str.length; i++) {
-			
-			if(i==0) {
+
+			if (i == 0) {
+				c++;
+			} else if (!str[i].equals(str[i - 1])) {
 				c++;
 			}
-			else if(!str[i].equals(str[i-1])) {
-				c++;
-			}
-			
+
 		}
-		
+
 //		System.out.println(c);
-		
-		String[]str1= new String[c];
-		int index=0;
+
+		String[] str1 = new String[c];
+		int index = 0;
 		for (int i = 0; i < str.length; i++) {
-			if(i==0) {
-				str1[index++]=str[i];
-			}else if(!str[i].equals(str[i-1])) {
-				str1[index++]=str[i];
+			if (i == 0) {
+				str1[index++] = str[i];
+			} else if (!str[i].equals(str[i - 1])) {
+				str1[index++] = str[i];
 			}
 		}
-		
+
 		System.out.println(Arrays.toString(str1));
 	}
 
