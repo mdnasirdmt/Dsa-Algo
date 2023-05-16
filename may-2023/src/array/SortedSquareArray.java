d92b6edff5066b6de22c835143d0a67f13ef431a
Md Nasir uddin <mdnasirdmt@gmail.com>
2023-05-16 07:24:43 +0530

    sorted aquare array

---
+++ may-2023/src/array/SortedSquareArray.java
@@ -0,0 +1,27 @@
+package array;
+
+import java.util.Arrays;
+
+public class SortedSquareArray {
+
+	public static void main(String[] args) {
+		int[]arr={4, 5, 6, 8, 9};
+		int l=0;
+		int r=arr.length-1;
+		int[]arr1=new int[arr.length];
+		for (int i = arr.length-1; i >=0; i--) {
+			if(Math.abs(arr[l])>arr[r]) {
+				arr1[i]=arr[l]*arr[l];
+				l++;
+			}else {
+				arr1[i]=arr[r]*arr[r];
+				r--;
+			}
+		}
+		for (int i = 0; i < arr.length; i++) {
+			arr[i]=arr1[i];
+		}
+		System.out.println(Arrays.toString(arr1));
+	}
+
+}
