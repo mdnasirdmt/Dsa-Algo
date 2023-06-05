ad85a54246b78a522f72c20014075bc2dc378859
Md Nasir uddin <mdnasirdmt@gmail.com>
2023-06-05 18:04:51 +0530

    spring security practice

---
+++ demo/src/main/java/com/config/JwtService.java
@@ -0,0 +1,7 @@
+package com.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+}
---
+++ demo/src/main/java/com/entity/Role.java
@@ -0,0 +1,7 @@
+package com.entity;
+
+public enum Role {
+
+    ADMIN,
+    USER
+}
