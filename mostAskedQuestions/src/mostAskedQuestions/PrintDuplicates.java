44d4c5cba7faf50822bafdf6766a90af9f7d6fd5
Md Nasir uddin <mdnasirdmt@gmail.com>
2023-09-05 12:38:56 +0530

    sum of natural number

--- mostAskedQuestions/src/mostAskedQuestions/PrintDuplicates.java
+++ mostAskedQuestions/src/mostAskedQuestions/PrintDuplicates.java
@@ -1,8 +1,16 @@
 package mostAskedQuestions;
 
+import java.util.Arrays;
+
 public class PrintDuplicates {
 	public static void main(String[] args) {
 		String str = "geekforgeeks";
+		char [] ch= str.toCharArray();
+		System.out.println(Arrays.toString(ch));
+		
+		for(int i= 0 ; i<str.length(); i++) {
+			
+		}
 
 	}
 
---
+++ sep-2023/src/apnaCollege/SumOfNaturalNumbers.java
@@ -0,0 +1,19 @@
+package apnaCollege;
+import java.util.*;
+public class SumOfNaturalNumbers {
+
+	public static void main(String[] args) {
+//		int[] arr = { 1, 2, 3, 4, 5 };
+		
+		Scanner n= new Scanner(System.in);
+		System.out.print(" Enter number to find sum of Natural number : ");
+		int num= n.nextInt();
+		int sum=0;
+		for (int i = 1; i <=num; i++) {
+			sum += i;
+		}
+		System.out.println(" Sum of natural numbers is : " + sum);
+
+	}
+
+}
