3f467fdb7215988d247cb47ff594afd92d114ca5
Md Nasir uddin <mdnasirdmt@gmail.com>
2023-09-07 10:49:37 +0530

    find power

--- sep-2023/src/mathsProblem/PowerOfNumber.java
+++ sep-2023/src/mathsProblem/PowerOfNumber.java
@@ -5,6 +5,15 @@ public class PowerOfNumber {
 		int base = 3;
 		int exponent = 3;
 
+		powerOfNuber(base, exponent);
+
+//		this is inbuilt way of finding power
+
+		double result = Math.pow(base, exponent);
+		System.out.println(base + " raised to the power of " + exponent + " is " + result);
+	}
+
+	private static void powerOfNuber(int base, int exponent) {
 		int pow = 1;
 		for (int i = 0; i < exponent; i++) {
 			pow *= base;
@@ -12,10 +21,6 @@ public class PowerOfNumber {
 
 		System.out.println("Power of " + base + " is " + pow);
 
-		//////////////////
-
-		double result = Math.pow(base, exponent);
-		System.out.println(base + " raised to the power of " + exponent + " is " + result);
 	}
 
 }
