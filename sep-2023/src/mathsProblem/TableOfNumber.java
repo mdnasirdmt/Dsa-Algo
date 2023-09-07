d56e6ca08ce78e33c57fe08e8ba182a120ef6be2
Md Nasir uddin <mdnasirdmt@gmail.com>
2023-09-07 12:25:49 +0530

    table of number

---
+++ sep-2023/src/mathsProblem/TableOfNumber.java
@@ -0,0 +1,19 @@
+package mathsProblem;
+
+public class TableOfNumber {
+	public static void main(String[] args) {
+		int n = 5;
+		int limit = 10;
+		
+		table(n, limit);
+	}
+
+	private static void table(int n, int limit) {
+
+		for (int i = 1; i <= limit; i++) {
+			System.out.println(n + " * " + i + " = " + +n * i);
+		}
+
+	}
+
+}
