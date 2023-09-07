013d29780d63a969e9f8038c22f7e5c2aa758e20
Md Nasir uddin <mdnasirdmt@gmail.com>
2023-09-07 10:15:34 +0530

    fid power of any number

--- sep-2023/src/mathsProblem/PowerOfNumber.java
+++ sep-2023/src/mathsProblem/PowerOfNumber.java
@@ -2,12 +2,20 @@ package mathsProblem;
 
 public class PowerOfNumber {
 	public static void main(String[] args) {
-		double base = 2.0;
-        double exponent = 3.0;
-        
-        double result = Math.pow(base, exponent);
-        
-        System.out.println(base + " raised to the power of " + exponent + " is " + result);
+		int base = 3;
+		int exponent = 3;
+
+		int pow = 1;
+		for (int i = 0; i < exponent; i++) {
+			pow *= base;
+		}
+
+		System.out.println("Power of " + base + " is " + pow);
+
+		//////////////////
+
+		double result = Math.pow(base, exponent);
+		System.out.println(base + " raised to the power of " + exponent + " is " + result);
 	}
 
 }
