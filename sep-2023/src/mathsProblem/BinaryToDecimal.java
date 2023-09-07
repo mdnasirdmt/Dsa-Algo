fe016365681371a6674c958fdba1370525b70ac7
Md Nasir uddin <mdnasirdmt@gmail.com>
2023-09-07 10:46:49 +0530

    binary to decimal

--- sep-2023/src/mathsProblem/BinaryToDecimal.java
+++ sep-2023/src/mathsProblem/BinaryToDecimal.java
@@ -5,27 +5,27 @@ public class BinaryToDecimal {
 	public static void main(String[] args) {
 
 		int n = 111;
+		binaryToDecimal(n);
+
+//		this is inbuilt way of converting to decimal
+		System.out.println("binary to decimal  =  " + Integer.parseInt("101", 2));
+
+	}
+
+	private static void binaryToDecimal(int n) {
 		String s = String.valueOf(n);
 
 		// Use Java Streams to reverse the string
 		String s1 = new StringBuilder(s).reverse().toString();
-//		System.out.print(s1);
 
 		String[] str = s1.split("");
-//		System.out.println(Arrays.toString(str));
 
 		int bin = 0;
 		for (int i = str.length - 1; i >= 0; i--) {
 			int digit = Integer.parseInt(str[i]);
-//			System.out.print(digit+ " ");
 			bin += Math.pow(2, i) * digit;
 		}
-		System.out.println("binary to decimal  =  "+bin);
-
-		////////////////////////////////
-
-//		this is inbuilt way of converting to decimal
-		System.out.println("binary to decimal  =  "+Integer.parseInt("101", 2));
+		System.out.println("binary to decimal  =  " + bin);
 
 	}
 
@@ -34,5 +34,3 @@ public class BinaryToDecimal {
 /*
  * convert binary number to decimal number example n= 1000 , output= 8
  */
-
-
