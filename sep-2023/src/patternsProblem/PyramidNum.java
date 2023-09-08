b750581c32a6a8f6572087a0ac053fa936adb32f
Md Nasir uddin <mdnasirdmt@gmail.com>
2023-09-08 14:26:49 +0530

    number pyramid half

--- sep-2023/src/patternsProblem/PyramidNum.java
+++ sep-2023/src/patternsProblem/PyramidNum.java
@@ -3,16 +3,16 @@ package patternsProblem;
 public class PyramidNum {
 
 	public static void main(String[] args) {
-		
+
 		pyramid(5, 5);
 
 	}
 
 	private static void pyramid(int row, int col) {
-		
-		for(int i=1; i<=row; i++) {
-			for(int j=1; j<=i; j++) {
-				System.out.print(i +" " );
+
+		for (int i = 1; i <= row; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(i + " ");
 			}
 			System.out.println();
 		}
