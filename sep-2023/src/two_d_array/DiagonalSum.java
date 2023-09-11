2bc9c017ed70aead6d414bae32aa8eb9ef11f0f7
Md Nasir uddin <mdnasirdmt@gmail.com>
2023-09-11 14:01:19 +0530

    sum of diagonals

---
+++ sep-2023/src/two_d_array/DiagonalSum.java
@@ -0,0 +1,80 @@
+package two_d_array;
+
+public class DiagonalSum {
+
+	public static void main(String[] args) {
+		int[][] mat = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+		// printing matrix values
+//		printMatrix(mat);
+
+		// print diagonal
+		printDigonal(mat);
+
+	}
+
+	private static void printDigonal(int[][] mat) {
+
+		// printing primary diagonal
+		System.out.println("primary diagonal ");
+
+		int sum = 0;
+
+		for (int i = 0; i < mat.length; i++) {
+
+			for (int j = 0; j < mat[i].length; j++) {
+
+				if (i == j) {
+
+					System.out.print(mat[i][j] + " ");
+					sum += mat[i][j];
+				}
+			}
+
+		}
+
+		// printing secondary diagonal
+		System.out.println();
+
+		System.out.println("secondary diagonal ");
+		for (int i = 0; i < mat.length; i++) {
+
+			for (int j = 0; j < mat[i].length; j++) {
+				if (i + j == mat[i].length - 1) {
+
+					System.out.print(mat[i][j] + " ");
+					sum += mat[i][j];
+				}
+			}
+
+		}
+		System.out.println();
+
+		System.out.println("sum of diagonals " + sum);
+
+	}
+
+	static void printMatrix(int[][] mat) {
+
+		for (int i = 0; i < mat.length; i++) {
+
+			for (int j = 0; j < mat[i].length; j++) {
+				System.out.print(mat[i][j] + " ");
+			}
+			System.out.println();
+
+		}
+
+		System.out.println("/////////////////");
+	}
+
+}
+//int[][] mat = { 
+//		{ 1, 2, 3 }, 
+//		{ 4, 5, 6 }, 
+//		{ 7, 8, 9 } };
+
+//diagonal values are 1,,5,,9 and 3,5,7
+//sum of diagonal 1 = 1+5+9= 15
+//sum of diagonal 2 = 3+7= 10
+//sum of diagonals = 25
