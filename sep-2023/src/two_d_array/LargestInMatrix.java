6e525816d92cdc5a16aa24b8f2cf06a074b9bb16
Md Nasir uddin <mdnasirdmt@gmail.com>
2023-09-11 12:01:58 +0530

    two d aray creation

---
+++ sep-2023/src/two_d_array/LargestInMatrix.java
@@ -0,0 +1,30 @@
+package two_d_array;
+
+import java.util.Scanner;
+
+public class LargestInMatrix {
+	public static void main(String[] args) {
+		int row= 3;
+		int col= 3;
+		
+		int[][] matrix = new int[row][col];
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println(" enter matrix values ");
+		for (int i = 0; i < matrix.length; i++) {
+
+			for (int j = 0; j < matrix[0].length; j++) {
+				matrix[i][j] = sc.nextInt();
+			}
+		}
+		
+		System.out.println("matrix values are as follows :");
+		for(int i= 0; i<matrix.length; i++) {
+			for(int j=0; j<matrix[0].length; j++) {
+				System.out.print(matrix[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
