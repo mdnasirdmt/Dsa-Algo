083d44bc8b90daa80258ef5f42529768651b3ddb
Md Nasir uddin <mdnasirdmt@gmail.com>
2023-09-15 12:41:43 +0530

    string first letter upper case

---
+++ sep-2023/src/string/UpperCase1stCharacter.java
@@ -0,0 +1,22 @@
+package string;
+
+public class UpperCase1stCharacter {
+
+	public static void main(String[] args) {
+		String s = "my name is nasir and i am a boy "; // output= My Name Is Nasir And I Am A Boy
+
+		String[] sArr = s.split(" ");
+
+		StringBuilder sb = new StringBuilder("");
+
+		for (int i = 0; i < sArr.length; i++) {
+
+			sb.append(Character.toUpperCase(sArr[i].charAt(0)));
+			sb.append(sArr[i].substring(1) + " ");
+
+		}
+
+		System.out.print(sb);
+	}
+
+}
