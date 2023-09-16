e7b161739529cfa842108eabf67b967b2ef85f70
Md Nasir uddin <mdnasirdmt@gmail.com>
2023-09-16 08:52:26 +0530

    | operator

---
+++ sep-2023/src/bit_manipulation/Bit_OR.java
@@ -0,0 +1,21 @@
+package bit_manipulation;
+
+public class Bit_OR {
+
+	public static void main(String[] args) {
+		int b = 5 | 6;
+
+		System.out.println(b);
+	}
+
+}
+
+//
+//bit OR operator
+// 
+//5 = 101
+//6 = 110
+//--------------
+//    111
+//    
+//  111 is 7 in decimal
