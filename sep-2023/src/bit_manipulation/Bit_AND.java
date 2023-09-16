a3810c6abaa40a6739db78a2452fc590a339a93e
Md Nasir uddin <mdnasirdmt@gmail.com>
2023-09-16 08:48:44 +0530

    & operator

---
+++ sep-2023/src/bit_manipulation/Bit_AND.java
@@ -0,0 +1,20 @@
+package bit_manipulation;
+
+public class Bit_AND {
+
+	public static void main(String[] args) {
+
+		int b = 5 & 1;
+
+		System.out.println(b);
+	}
+
+}
+
+//binary to decimal= 
+//5 = 101
+//6 = 110
+//-------------
+//    100
+//    
+//100  = 4 in decimal
