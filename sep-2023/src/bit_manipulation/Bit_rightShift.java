ea4b10d0311608bf9371f4f9d98ca0492bbf9542
Md Nasir uddin <mdnasirdmt@gmail.com>
2023-09-16 10:56:22 +0530

    >> operator

--- sep-2023/src/bit_manipulation/Bit_rightShift.java
+++ sep-2023/src/bit_manipulation/Bit_rightShift.java
@@ -3,8 +3,10 @@ package bit_manipulation;
 public class Bit_rightShift {
 
 	public static void main(String[] args) {
-		int b= 5>>2;
-		
+		int b = 5 >> 2;
+
 		System.out.println(b);
+
+		System.out.println(5 / (2 * 2));
 	}
 }
